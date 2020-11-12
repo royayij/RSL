b42beec1fa510f4fe3e3c6b10fdc117a067350a5
Roya <rnaser917@gmail.com>
2020-11-12 15:30:57 +0330

    edit main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from roya import  print_roya
 from samira import samira_print
 from leyla import print_leyla
+def main():
+    print_roya()
+    print_leyla()
+    samira_print()
 
-print_roya()
-print_leyla()
-samira_print()
+if __name__ == '__main__':
+    main()
